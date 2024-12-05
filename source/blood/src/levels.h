c4472b40083e635ad015259fdef49dc2d6a017c7
Emile Belanger <emile.belanger@gmail.com>
2024-12-05 10:47:08 +0000

    Revert back to 16 levels as crashes some mods

--- source/blood/src/levels.h
+++ source/blood/src/levels.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #define kMaxMessages 32
 #define kMaxEpisodes 7
-#define kMaxLevels 60
+#define kMaxLevels 16
 
 #pragma pack(push, 1)
 
